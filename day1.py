9f4673d217d6d13992b61a04dbc749a1aeee76c0
eirinko <eirinkorvald.ek@gmail.com>
2023-12-04 20:22:23 +0100

    Got the right answer for part 2!

--- day1.py
+++ day1.py
@@ -5,9 +5,6 @@ file = open("day1input.txt","r")
 content=file.readlines()
 file.close()
 
-#Test list below should give a result of 281.
-#content = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
-
 result = 0
 
 #PART 1:
@@ -24,26 +21,27 @@ result = 0
 #PART 2: 
 stringnumbers = ["one","two","three","four","five","six","seven","eight","nine","1","2","3","4","5","6","7","8","9"]
 numbers = ["1","2","3","4","5","6","7","8","9","1","2","3","4","5","6","7","8","9"]
+
 for line in content:
     numberindexfromleft = []
     numberindexfromright = []
     
     for word in stringnumbers:
         wordLeftIndex = line.find(word) #Becomes -1 if the line doesn't contain the word.
-
+        
         if (wordLeftIndex ==-1):
-            numberindexfromleft.append(len(stringnumbers)-1)
+            numberindexfromleft.append(len(line))
         else:
             numberindexfromleft.append(wordLeftIndex)
 
         numberindexfromright.append(line.rfind(word))
-
+        
     left = numberindexfromleft.index(min(numberindexfromleft))
     right = numberindexfromright.index(max(numberindexfromright))
     
     charNumber = numbers[left] + numbers[right]
-    
     number = int(charNumber)
+    
     result += number
     
 print(result)
